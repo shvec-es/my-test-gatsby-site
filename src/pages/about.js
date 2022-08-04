03df5b3c60a5bfa4c0a5c0d73d59959da92a7706
shvec-es <kotova.es.1510@gmail.com>
2022-08-04 16:52:58 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import Layout from "../components/layot";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
   return (
@@ -13,3 +12,7 @@ const AboutPage = () => {
 };
 
 export default AboutPage;
+
+// export const Head = () => (
+//   <link href="/dist/output.css" rel="stylesheet"></link>
+// );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import Layout from "../components/layot";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Cute little cat" src="../images/cat1.jpeg" />
     </Layout>
   );
 };
 
 export default IndexPage;
+
+// export const Head = () => (
+//   <link href="/dist/output.css" rel="stylesheet"></link>
+// );
