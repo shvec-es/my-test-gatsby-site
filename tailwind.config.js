53ed6a22350f834b3716e6d43f5fdb299dfc5427
shvec-es <kotova.es.1510@gmail.com>
2022-08-04 21:56:37 +0300

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
