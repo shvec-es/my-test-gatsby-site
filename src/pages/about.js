9a2ede66bbb099719f31301e59ad2f702a137449
shvec-es <kotova.es.1510@gmail.com>
2022-08-05 10:06:44 +0300

    Add form

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
 import * as React from "react";
+import { useForm } from "react-hook-form";
 import Layout from "../components/layout";
 
 const AboutPage = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Layout pageTitle="About Me">
-      <p>
-        Hi there! I'm the proud creator of this site, which I built with Gatsby.
-      </p>
+      <p>Enter your name and phone number and we'll call you in 10 minutes!</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("name", { required: true, maxLength: 20 })}
+          className="w-full mb-1 border "
+        />
+        <input
+          type="tel"
+          {...register("phone", { required: true })}
+          className="w-full mb-1 border "
+        />
+        <input type="submit" className="bg-indigo-500 rounded-md" />
+      </form>
     </Layout>
   );
 };
