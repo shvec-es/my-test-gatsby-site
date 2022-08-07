68f3596a753a0c5d9c77de942992bfc3feb85aa5
shvec-es <kotova.es.1510@gmail.com>
2022-08-07 09:45:33 +0300

    add thank-you page

---
+++ src/pages/thank-you.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ThankYouPage = () => {
+  return (
+    <h2 className="bg-green-300 text-center mt-15">
+      Thank you for message, our meneger will call you in 10 minutes!
+    </h2>
+  );
+};
+
+export default ThankYouPage;
